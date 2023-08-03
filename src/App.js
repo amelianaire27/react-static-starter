db38ddcf1b225bb87966f9190cb26813e4af3fe5
Amelia <amelia.rae27@gmail.com>
2023-08-03 13:54:35 -0700

    Added the commandbar in app instead

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Routes from 'react-static-routes'
 import useCommandBar from './useCommandBar'
 import './app.css'
 
-export default () => (
+function App () {
+  useCommandBar();
   <Router>
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
-      <div className="content">
-        <Routes />
-      </div>
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/blog">Blog</Link>
+    </nav>
+    <div className="content">
+      <Routes />
     </div>
+   </div>
   </Router>
-)
+}
+
+export default App;

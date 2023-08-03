191379aaef82181a7ea057ca7993a89c95169cb9
Amelia <amelia.rae27@gmail.com>
2023-08-03 14:04:17 -0700

    Trying it again

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import React from 'react'
 import { Router, Link } from 'react-static'
 //
 import Routes from 'react-static-routes'
-import useCommandBar from './useCommandBar'
+
 import './app.css'
 
-function App () {
-  useCommandBar();
-  <Router>
-  <div>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/blog">Blog</Link>
-    </nav>
-    <div className="content">
-      <Routes />
-    </div>
-   </div>
-  </Router>
+import { init } from 'commandbar';
+init('81136260');
+
+const App = ({ children }) => {
+  useEffect(() => {
+    const loggedInUserId = '12345'; // example
+    window.CommandBar.boot(loggedInUserId).then(() => {
+      <Router>
+         <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blog">Blog</Link>
+          </nav>
+          <div className="content">
+            <Routes />
+          </div>
+        </div>
+      </Router>
+    });
+
+    return () => {
+      window.CommandBar.shutdown();
+    }
+  }, []);
 }
 
 export default App;
